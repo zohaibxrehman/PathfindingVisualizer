513fe3aad4b80b0ec1013c0cc164a4f4b831c453
zohaibxrehman <zohaibxrehman@gmail.com>
2020-06-26 00:43:58 -0400

    Added navbar icon. Navbar stylistic changes.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,12 @@ export class NavBar extends Component {
 		return (
 			<div>
 				<Navbar bg="dark" variant="dark">
-					<Navbar.Brand href="#home">Pathfinder.algorithm</Navbar.Brand>
+					<Navbar.Brand href="#home">
+						<img alt="" src="/maze.svg" width="35" height="35" className="d-inline-block align-top" />
+						{'  '}
+						PATHFINDER{' '}
+						<small style={{ color: '#eee' }}>Pathfinding and Maze Generation Algorithm Visualizer</small>
+					</Navbar.Brand>
 				</Navbar>
 			</div>
 		);
