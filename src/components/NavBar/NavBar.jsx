cd50a263b3d97e9aea520d344747883c05a1edf7
zohaibxrehman <zohaibxrehman@gmail.com>
2020-09-24 20:28:31 -0400

    Added build to gitignore.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Navbar } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import navStyle from './NavBar.module.css';
+import logo from './maze.svg';
 
 export class NavBar extends Component {
 	render() {
@@ -13,7 +14,7 @@ export class NavBar extends Component {
 							href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap"
 							rel="stylesheet"
 						/>
-						<img alt="" src="/maze.svg" width="35" height="35" className="d-inline-block align-top" />
+						<img alt="" src={logo} width="35" height="35" className="d-inline-block align-top" />
 						{'  '}
 						<span className={navStyle.navBar}>PATHFINDER </span>
 						<small style={{ color: '#eee' }}>
